5e72f040b4358dd9d227b8a7a4ab0a8cd7adf4d4
Spencer Adams <sadams4123@gmail.com>
2019-09-17 21:11:00 -0600

    Real project not just the swift files
    Real project not just the swift files

---
+++ Assignment2/FreedomWithFinance/UserLogController.swift
@@ -0,0 +1,13 @@
+//
+//  UserLogController.swift
+//  FreedomWithFinance
+//
+//  Created by Spencer Adams on 9/17/19.
+//  Copyright © 2019 Spencer Adams. All rights reserved.
+//
+
+import Foundation
+
+class UserLogController: NSObject {
+
+}
